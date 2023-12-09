0a45969cb91176d03fb61a4895789c5d15f2a8b8
CodeNinja1989 <153234904+CodeNinja1989@users.noreply.github.com>
2023-12-09 09:21:19 -0600

    Add files via upload

---
+++ CoffeeShopGroupProject/js/index.js
@@ -0,0 +1,17 @@
+﻿// Get a reference to the button, select element, and the message div
+const orderButton = document.getElementById("orderButton");
+const itemSelect = document.getElementById("itemSelect");
+const messageDiv = document.getElementById("message");
+
+// Add a click event listener to the button
+orderButton.addEventListener("click", function () {
+    // Get the selected item from the select element
+    const selectedItem = itemSelect.value;
+
+    // Display a message in the message div when the button is clicked
+    messageDiv.textContent = `Thank you for placing your order for a ${selectedItem}!`;
+});
+
+
+
+
